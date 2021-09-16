508c423ddfb2c3470e7811398107ac78f2e95296
HAYASHI65 <hayashim853@gmail.com>
2021-09-16 12:10:19 +0900

    チーム新規投稿機能の実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,22 @@ class TeamsController < ApplicationController
   def index
     @teams = Team.all
   end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def team_params
+    params.require(:team).permit(:image, :team_name, :sports_id, :group_id, :active_area_id, :active_day_id, :active_time_id, :place, :gender_ratio, :level_id, :profile).merge(user_id: current_user.id)
+  end
 end
