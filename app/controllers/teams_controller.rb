8afd77ca0081e948994b7faaec9dfd82bfaec247
HAYASHI65 <hayashim853@gmail.com>
2021-09-16 12:17:35 +0900

    チーム編集機能の実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -20,6 +20,19 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:id])
   end
 
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if @team.update(team_params)
+      redirect_to team_path(@team.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def team_params
     params.require(:team).permit(:image, :team_name, :sports_id, :group_id, :active_area_id, :active_day_id, :active_time_id, :place, :gender_ratio, :level_id, :profile).merge(user_id: current_user.id)
