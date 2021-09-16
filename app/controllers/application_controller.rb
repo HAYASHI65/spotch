d75dc1c0b6a121043192c4c7ca939a2577c34a8c
HAYASHI65 <hayashim853@gmail.com>
2021-09-16 10:35:40 +0900

    deviseのユーザー管理機能の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :gender_id, :birthday, :nickname,
+                                             :sports_id, :active_area_id, :active_day_id, :active_time_id, :achievement_id, :achievement_content, :profile])
+  end
 end
