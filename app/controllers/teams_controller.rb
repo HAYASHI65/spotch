95a1d07466515567c8869adb737e5e6949203a40
HAYASHI65 <hayashim853@gmail.com>
2021-09-17 12:48:15 +0900

    レイアウトの修正

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,5 @@
 class TeamsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :set_team, only: [:show, :edit, :update, :destroy]
   before_action :search_team, only: [:presearch, :search]
